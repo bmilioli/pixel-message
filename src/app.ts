11b7e8d781bbfad8cffe3f32cef9fab7649e7b68
Bmilioli <brunomiliolif@gmail.com>
2024-05-12 22:19:57 -0300

    Arquchiteture

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,25 @@
-import express, { NextFunction, Request, Response } from "express";
-import config from "../config/config";
+import express, { NextFunction, Request, Response } from 'express';
+import config from '../config/config';
 
-import whatsappRouter from "./routes/whatsapp.route";
+import whatsappRouter from './routes/whatsapp.route';
 
 const app = express();
 const port = config.port;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.get('/', (req: any, res: Response) => {
+  res.send('Hello, world!');
 });
 
 //Global error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  err.status = err.status || "error";
+  err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
   res
     .status(err.statusCode)
     .json({ status: err.status, message: err.message, code: err?.shortCode });
 });
 
-app.use("/whatsapp", whatsappRouter);
+app.use('/whatsapp', whatsappRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/routes/whatsapp.route.ts
+++ src/routes/whatsapp.route.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, world!");
+router.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
 export default router;
